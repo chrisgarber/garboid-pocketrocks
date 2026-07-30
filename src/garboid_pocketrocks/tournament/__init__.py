64e3a0263300f43afa0eb9a684a1b7c9d6f9d062
Chris Garber <christopher.garber@braze.com>
2026-07-29 20:08:19 -0400

    feat: fit tie-aware Plackett-Luce bot ratings

--- src/garboid_pocketrocks/tournament/__init__.py
+++ src/garboid_pocketrocks/tournament/__init__.py
@@ -1,5 +1,15 @@
 """Deterministic multiplayer bot tournaments and strength analysis."""
 
+from garboid_pocketrocks.tournament.rating import (
+    PLBotRating,
+    PLFitDiagnostics,
+    PlackettLuceFit,
+    RankingObservation,
+    TiePrevalence,
+    TournamentRatingError,
+    fit_plackett_luce,
+    observations_from_games,
+)
 from garboid_pocketrocks.tournament.schedule import (
     ConditionQuota,
     PairExposure,
@@ -10,8 +20,16 @@ from garboid_pocketrocks.tournament.schedule import (
 
 __all__ = [
     "ConditionQuota",
+    "PLBotRating",
+    "PLFitDiagnostics",
     "PairExposure",
+    "PlackettLuceFit",
+    "RankingObservation",
+    "TiePrevalence",
     "TournamentConfig",
     "TournamentPlan",
     "TournamentPlanner",
+    "TournamentRatingError",
+    "fit_plackett_luce",
+    "observations_from_games",
 ]
